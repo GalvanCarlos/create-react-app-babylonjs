fae0449ee599878d44c2dcb61a9fb2b8407759b6
Carlos <galvancarlosjr@gmail.com>
2021-03-01 01:33:55 -0600

    Model Link Test

--- src/withModel.js
+++ src/withModel.js
@@ -86,7 +86,7 @@ const WithModel = props => {
               <arcRotateCamera name="camera1" alpha={Math.PI / 2} beta={Math.PI / 2} radius={9.0} target={Vector3.Zero()} minZ={0.001} />
               <hemisphericLight name="light1" intensity={0.7} direction={Vector3.Up()} />
               
-              <ScaledModelWithProgress rootUrl={`${baseUrl}BoomBox/glTF/`} sceneFilename="BoomBox.gltf" scaleTo={3} 
+              <ScaledModelWithProgress rootUrl={`src/scene.gltf`} sceneFilename="scene.gltf" scaleTo={3} 
                 progressBarColor={Color3.FromInts(255, 165, 0)} center={new Vector3(2.5, 0, 0)}
                 onModelLoaded={onModelLoaded}
               />
